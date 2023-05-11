6ed18aa310e95b8645dc94af390f1b9ae2cd2838
adiflp <adifulop23@gmail.com>
2023-05-11 13:40:43 +0200

    mergeado funcionando

--- src/router/routes/usuario.js
+++ src/router/routes/usuario.js
@@ -31,10 +31,6 @@ export default [
         ],
       },
     },
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 3e75fdf1b131b5500431c15d4e6090f5923a85f0
     {
       path: '/users/usuarios/bbbdd',
       name: 'users-usuarios-bbdd',
@@ -117,9 +113,4 @@ export default [
       },
       
     },
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/master
->>>>>>> 3e75fdf1b131b5500431c15d4e6090f5923a85f0
 ]

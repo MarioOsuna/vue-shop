4e3b2f8519ce2e610cc71da07f7e3f2ad6c6505c
adiflp <adifulop23@gmail.com>
2023-05-11 13:31:49 +0200

    todo funcionando

--- src/views/usuarios/usuariosBBDD.vue
+++ src/views/usuarios/usuariosBBDD.vue
@@ -6,30 +6,116 @@
                 <div class="ecommerce-header-items">
                     <div class="view-options d-flex">
                         <!-- Botones Vistas -->
-                        <b-tabs>
-                            <!-- Tab: Vista Tabla -->
-                            <b-tab active>
-                                <template #title>
-                                    <feather-icon icon="MenuIcon" size="16" class="mr-0 mr-sm-50" />
-                                </template>
-                                <usuariosBBDDTabla :user="users" @refresh="getData()" />
+                        <b-overlay :show="show" rounded="sm">
+                            <b-tabs>
+                                <!-- Tab: Vista Tabla -->
+                                <b-tab active>
+                                    <template #title>
+                                        <feather-icon icon="MenuIcon" size="16" class="mr-0 mr-sm-50" />
+                                    </template>
+                                    <usuariosBBDDTabla :user="users" @refresh="getData()" />
 
-                            </b-tab>
-                            <!-- Tab: Vista Cards -->
-                            <b-tab>
-                                <template #title>
-                                    <feather-icon icon="GridIcon" size="16" class="mr-0 mr-sm-50" />
-                                </template>
-                                <usuariosBBDDCards :user="users" @refresh="getData()" />
-
-                            </b-tab>
-                        </b-tabs>
+                                </b-tab>
+                                <!-- Tab: Vista Cards -->
+                                <b-tab>
+                                    <template #title>
+                                        <feather-icon icon="GridIcon" size="16" class="mr-0 mr-sm-50" />
+                                    </template>
+                                    <usuariosBBDDCards :user="users" @refresh="getData()" />
+                                </b-tab>
+                            </b-tabs>
+                        </b-overlay>
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <br>
+    <div>
+
+        <b-sidebar ref="mySidebar" id="sidebar-right" bg-variant="white" right backdrop shadow>
+            <b-overlay :show="show" rounded="sm">
+                <template>
+                    <b-form id="miform" @submit.prevent="agregarRegistro">
+
+                        <div class="m-2">
+                            <div class="text-center mb-3">
+                                <!-- Name -->
+                                <b-card-text class="mt-2 h4 color-inherit text-reset">
+                                    Crear Usuarios
+                                </b-card-text>
+                            </div>
+                            <b-row>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Usuario" label-for="usuario">
+                                        <b-form-input placeholder="alex1234" id="usuario" v-model="users.usuario" required />
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Nombre" label-for="nombre">
+                                        <b-form-input placeholder="Francisco" id="nombre" v-model="users.nombre" required />
+                                    </b-form-group>
+                                </b-col>
+                            </b-row>
+                            <b-row>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Apellido" label-for="apellido">
+                                        <b-form-input placeholder="Sanchez" id="apellido" v-model="users.apellido" required />
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Email" label-for="email">
+                                        <b-form-input type="email" placeholder="francis@example.com" id="email" v-model="users.email" />
+                                    </b-form-group>
+                                </b-col>
+                            </b-row>
+                            <b-row>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Teléfono" label-for="telefono">
+                                        <b-form-input type="number" placeholder="675890123" id="telefono" v-model="users.telefono" />
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Ciudad" label-for="ciudad">
+                                        <b-form-input placeholder="Madrid" id="ciudad" v-model="users.ciudad" required />
+                                    </b-form-group>
+                                </b-col>
+                            </b-row>
+                            <b-row>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Dirección" label-for="direccion">
+                                        <b-form-input placeholder="Calle Borja Pavon" id="direccion" v-model="users.direccion" />
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="12" md="6">
+                                    <b-form-group label="Número" label-for="numero">
+                                        <b-form-input type="number" placeholder="2" id="numero" v-model="users.numero" />
+                                    </b-form-group>
+                                </b-col>
+                            </b-row>
+                            <b-row class="justify-content-center">
+                                <b-col>
+                                    <b-form-group label="Código Postal" label-for="codigo_postal" style="text-align: center;">
+                                        <b-form-input type="number" placeholder="14700" id="codigo_postal" v-model="users.codigo_postal" required />
+                                    </b-form-group>
+                                </b-col>
+                            </b-row>
+                            <b-row class="justify-content-center m-1">
+                                <b-button @click=completado type="submit" v-ripple.400="'rgba(113, 102, 240, 0.15)'" variant="primary" class="mb-1 mb-sm-0 mr-0 mr-sm-1">
+                                    Crear Usuario
+                                </b-button>
+
+                            </b-row>
+                            <b-row class="justify-content-center m-1">
+                                <b-button type="reset" v-ripple.400="'rgba(113, 102, 240, 0.15)'" variant="secondary" class="mb-1 mb-sm-0 mr-0 mr-sm-1" style=""> Resetear Campos</b-button>
+                            </b-row>
+                        </div>
+                    </b-form>
+                </template>
+            </b-overlay>
+        </b-sidebar>
+    </div>
+
 </div>
 </template>
 
@@ -125,21 +211,24 @@ export default {
     methods: {
         eliminarValor(id) {
             fetch('https://vueproyect.000webhostapp.com/vue_project.php/?borrar=' + id, {
-                    method: "DELETE",
+                    method: "POST",
                 })
                 .then((resp) => {
                     console.log(resp)
                     console.log("Usuario Eliminado Correctamente")
                     // location.reload()
-                    this.getData()
+                    // this.getData();
                 })
 
         },
         getData() {
+            this.show = true;
             fetch('https://vueproyect.000webhostapp.com/vue_project.php/')
                 .then(response => response.json())
                 .then(responseData => {
+
                     this.users = responseData;
+                    this.show = false;
                 })
         },
 
@@ -156,20 +245,23 @@ export default {
                 numero: this.users.numero,
                 codigo_postal: this.users.codigo_postal
             }
-
-            fetch('https://vueproyect.000webhostapp.com/vue_project.php/?insertar=1',{
+            this.show = true;
+            fetch('https://vueproyect.000webhostapp.com/vue_project.php/?insertar=1', {
                     method: "POST",
                     body: JSON.stringify(datosEnviar),
                 })
                 .then((resp) => {
+
                     console.log(resp)
                     console.log("Actualizado Correctamente")
                     console.log(resp)
                     // location.reload()
                     // this.crearUsuarioToast('primary')
                     this.$refs.mySidebar.hide();
-                    this.$emit('refresh');
+                    this.show = false;
+                    this.getData();
                     this.resetearDatos();
+
                 })
 
         },
